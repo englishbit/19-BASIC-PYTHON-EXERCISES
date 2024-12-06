7d81cb6236b7225dd0e84c04d3d89fccb6f3097a
rk <englishenglishenglish@proton.me>
2024-12-06 11:47:19 +0000

    Set up simple calculator project

---
+++ simple_calculator.py
@@ -0,0 +1,9 @@
+#this is simple calculator with two variable 
+num1=34
+num2=3
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+mod=num1%num2
+print(f"the addition:{add}\nthe subtraction:{sub}\nthe multiplication:{mul}\nthe division:{div}\nthe modulus:{mod}")
