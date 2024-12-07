792c97a44ee35af318c5a9b13ea521b8a477f39e
rk <englishenglishenglish@proton.me>
2024-12-07 16:23:24 +0000

    Program to find the distance between two points

---
+++ distance_between_two_points.py
@@ -0,0 +1,10 @@
+import math
+x1=float(input("enter x1 for p1 "))
+y1=float(input("enter y1 for p1 "))
+
+x2=float(input("enter x2 for p2 "))
+y2=float(input("enter y2 for p2 "))
+
+re=math.sqrt((x2-x1)**2+(y2-y1)**2)
+print(re)
+
