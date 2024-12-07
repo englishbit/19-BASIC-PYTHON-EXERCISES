b30d77d1783eb445b39ea7e634f0ac3e43a45f67
rk <englishenglishenglish@proton.me>
2024-12-07 16:33:23 +0000

    Update time_to_sec_convertor.py

--- time_to_sec_convertor.py
+++ time_to_sec_convertor.py
@@ -1,8 +1,9 @@
 day=int(input("enter the day:"))
 hours=int(input("enter the hours:"))
 minute=int(input("enter the minute:"))
-dy_to_mnt=24*60
-hrs_to_mnt=hours*60
-second=(minute+dy_to_mnt+hrs_to_mnt)*60
+
+dy_to_hr=day*24
+hrs_to_mnt=(hours+dy_to_hr)*60
+second=(minute+hrs_to_mnt)*60
 print(second)
 
