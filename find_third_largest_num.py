839068015dc658f92c44105caeb23eff8f1d10cb
rk <englishenglishenglish@proton.me>
2024-12-10 14:59:10 +0000

    Programm to find the third largest number

---
+++ find_third_largest_num.py
@@ -0,0 +1,88 @@
+
+def third_larg_num(num1,num2,num3,num4):
+    
+    
+    #first step
+    if num1>=num2 and num1>=num3 and num1>=num4:
+        if num2>=num3 and num2>=num4:
+            if num3>=num4:
+                third_largest=num3
+            else:
+                third_largest=num4
+        elif num3>=num2 and num3>=num4:
+            if num2>=num4:
+                third_largest=num2
+            else:
+                third_largest=num4
+        else:
+            
+                if num2>=num3:
+                    third_largest=num2
+                else:
+                    third_largest=num3  
+                    
+                    
+                    
+    #second step          
+                      
+    elif num2>=num1 and num2>=num3 and num2>=num4:
+        if num1>=num3 and num1>=num4:
+            if num3>=num4:
+                third_largest=num3
+            else:
+                third_largest=num4
+        elif num3>=num1 and num3>=num4:
+            if num1>=num4:
+                third_largest=num1
+            else:
+                third_largest=num4 
+        else:
+             
+             if num1>=num3:
+                 third_largest=num1
+             else:
+                 third_largest=num3  
+            
+    #third step
+    elif num3>=num1 and num3>=num2 and num3>=num4:
+        if num1>=num2 and num1>=num4:
+            if num2>=num4:
+                third_largest=num2
+            else:
+                third_largest=num4      
+        elif num2>=num1 and num2>=num4:
+            if num1>=num4:
+                third_largets=num1
+            else:
+                third_largest=num4
+            
+        else:
+            if num1>=num2:
+                third_largest=num1
+            else:
+                third_largest=num2
+                
+                
+    #final num
+                     
+    else:
+        if num1>=num2 and num1>=num3:
+            if num2>=num3:
+                third_largest=num2
+            else:
+                third_largest=num3
+        elif num2>=num1 and num2>=num3:
+            if num1>=num3:
+                third_largest=num1
+            else:
+                third_largest=num3
+        else:    
+            
+            if num1>=num2:
+                third_largest=num1
+            else:
+                third_largest=num2
+    return third_largest
+
+
+print(third_larg_num(33,220,62,30))
