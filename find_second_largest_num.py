09ad50810dba3f0547a731868f252b2cb73ecd20
rk <englishenglishenglish@proton.me>
2024-12-10 11:16:08 +0000

    programm to find the second largest number

---
+++ find_second_largest_num.py
@@ -0,0 +1,29 @@
+def find_sec_larg(num1,num2,num3):
+    if num1>=num2 and num1>=num3:
+        
+        if num2>=num3:
+            sec_largest=num2
+            thrd_largest=num3
+        else:
+            sec_largest=num3
+            thrd_largest=num2
+    elif num2>=num1 and num2>=num3:
+        
+        if num1>=num3:
+            sec_largest=num1
+            thrd_largest=num3
+        else:
+            sec_largest=num3
+            thrd_largest=num1
+    else:
+        
+        if num2>=num1:
+            sec_largest=num2
+            thrd_largest=num1
+        else:
+            sec_largest=num1
+            thrd_largest=num2  
+    return sec_largest      
+                 
+
+print(find_sec_larg(5,3,0))
