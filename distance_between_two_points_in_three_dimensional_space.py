9de5be3e17d601acfbadec83c5f3e76d62afd034
rk <englishenglishenglish@proton.me>
2024-12-10 16:31:28 +0000

    finding  distance between two points in two-dimensional space

---
+++ distance_between_two_points_in_three_dimensional_space.py
@@ -0,0 +1,12 @@
+import math
+x1=float(input())
+y1=float(input())
+z1=float(input())
+
+x2=float(input())
+y2=float(input())
+z2=float(input())
+
+re=math.sqrt(((x2-x1)**2)+((y2-y1)**2)+((z2-z1)**2))
+print(re)
+
