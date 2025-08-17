68a4ccc93b0a5c9e523352ef852cce0590c62b7e
SK <englishenglishenglish@proton.me>
2025-08-17 13:40:33 +0000

    Update 11-Making a simple calculator.py

--- 11-Making a simple calculator.py
+++ 11-Making a simple calculator.py
@@ -1,9 +1,11 @@
-#this is simple calculator with two variable 
+#logic for simple calculator
 num1=34
 num2=3
+#logic for basic calculations
 add=num1+num2
 sub=num1-num2
 mul=num1*num2
 div=num1/num2
 mod=num1%num2
 print(f"the addition:{add}\nthe subtraction:{sub}\nthe multiplication:{mul}\nthe division:{div}\nthe modulus:{mod}")
+
