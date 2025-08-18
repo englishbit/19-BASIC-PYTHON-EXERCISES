4f746c5c235498e86e2237b3d69959ce069d72fc
SK <englishenglishenglish@proton.me>
2025-08-18 13:53:48 +0000

    Update 04-Finding the distance between two points.py

--- 04-Finding the distance between two points.py
+++ 04-Finding the distance between two points.py
@@ -6,8 +6,9 @@ y1=float(input("enter y1 for p1 "))
 x2=float(input("enter x2 for p2 "))
 y2=float(input("enter y2 for p2 "))
 
-#logic for calculating sqrt
+#logic for calculating and printing output
 re=math.sqrt((x2-x1)**2+(y2-y1)**2)
 print(re)
+
 
 
