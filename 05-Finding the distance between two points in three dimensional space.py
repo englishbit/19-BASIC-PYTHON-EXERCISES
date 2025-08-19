5e0acfedf8c26dc5635abedfd1cb2ce1d6eb1523
SK <englishenglishenglish@proton.me>
2025-08-19 12:24:27 +0000

    Update 05-Finding the distance between two points in three dimensional space.py

--- 05-Finding the distance between two points in three dimensional space.py
+++ 05-Finding the distance between two points in three dimensional space.py
@@ -3,12 +3,13 @@ import math
 x1=float(input())
 y1=float(input())
 z1=float(input())
-
+#logic for taking input
 x2=float(input())
 y2=float(input())
 z2=float(input())
 
 re=math.sqrt(((x2-x1)**2)+((y2-y1)**2)+((z2-z1)**2))
 print(re)
+
 
 
