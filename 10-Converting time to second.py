f76690b12d2d3827e71d266f6d91c7761343962f
SK <englishenglishenglish@proton.me>
2025-08-22 08:21:29 +0000

    Update 10-Converting time to second.py

--- 10-Converting time to second.py
+++ 10-Converting time to second.py
@@ -3,9 +3,11 @@ day=int(input("enter the day:"))
 hours=int(input("enter the hours:"))
 minute=int(input("enter the minute:"))
 
+#logic for calculation
 dy_to_hr=day*24
 hrs_to_mnt=(hours+dy_to_hr)*60
 second=(minute+hrs_to_mnt)*60
 print(second)
+
 
 
